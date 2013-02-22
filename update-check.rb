61d4f226f8ed3761de4be04216b93cc99d04da0a
Stephen Connolly <stephen.alan.connolly@gmail.com>
2013-02-22 12:03:49 +0000

    Update update-check

--- update-check.rb
+++ update-check.rb
@@ -6,10 +6,13 @@ require 'open-uri'
 
 current = Gem::Version.new((Nokogiri::HTML(open('http://backbonejs.org/')).css('span[class="version"]').first.content().match /\((.*)\)/).captures[0])
 
-maven = Gem::Version.new(Nokogiri::XML(File.open("pom.xml")).remove_namespaces!.xpath("/project/version").first.content().sub /-SNAPSHOT/,"")
+maven = Gem::Version.new(Nokogiri::XML(File.open(File.expand_path('../pom.xml', __FILE__))).remove_namespaces!.xpath("/project/version").first.content().sub /-SNAPSHOT/,"")
+
+puts "["+File.dirname(__FILE__)+"]   Last redist: " + maven.to_s
+puts "["+File.dirname(__FILE__)+"] Last upstream: " + current.to_s
 
 if current >= maven
-puts "A release can be made"
-exit 1 
+	puts "["+File.dirname(__FILE__)+"] A release can be made!!!"
+	exit 1 
 end
-puts "Everything up to date"
+puts "["+File.dirname(__FILE__)+"] Everything up to date."
